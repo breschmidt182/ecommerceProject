6176307b5bd998625d4c30cae5a60bee9b551424
Bre <breanna.schmidt12@gmail.com>
2023-09-27 15:34:44 -0400

    added gitignore

--- server/server.js
+++ server/server.js
@@ -3,13 +3,26 @@ const env = require('dotenv');
 const app = express();
 const mongoose = require("mongoose");
 
+
 const PORT = process.env.PORT || 3002;
-const MONGODB_URI = process.env.MONGODB_URI || "";
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.wtaxbyl.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 env.config();
 
+// mongodb connection
+// mongodb+srv://breannaschmidt12:vTehdZWXbfN0WUHN@cluster0.wtaxbyl.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(MONGODB_URI,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}
+).then( () => {console.log('Database is connected! :)')});
+
 app.use(express.json());
-app.use(require("./routes/api.js"))
+// app.use(require("./routes/api.js"))
 
 // app.get('/', (req, res, next) => {
 // 	res.status(200).json({
